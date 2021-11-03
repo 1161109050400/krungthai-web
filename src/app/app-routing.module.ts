3dcb8eb47bf97d227b79140b0546385c02cfba87
DESKTOP-AOCUDPL\Ammber <ammekcomsci@gmail.com>
2021-11-03 10:28:55 +0700

    add design responsive login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormAxaComponent } from './module/form-axa/form-axa.component';
+import { LoginComponent } from './module/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full' , redirectTo: 'home-page'},
+  {path: 'login', component:LoginComponent},
+  {path: 'krungthai/form-axa' , component:FormAxaComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
